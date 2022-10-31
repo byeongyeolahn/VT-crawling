c56b614238a18d64b83e34a78ea4443fd679856f
byeongyeol <101905747+byeongyeolahn@users.noreply.github.com>
2022-11-01 03:38:19 +0900

    Update main_crawling.py
    Shadow DOM 요소로 인해 기존 Selenium 방식은 동작하지 않음.
    pyshadow를 사용하여 해결

--- main_crawling.py
+++ main_crawling.py
@@ -2,35 +2,54 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time 
+from pyshadow.main import Shadow
+import time
+import os
 
-def main_crawling():
-    options = Options()
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-    options.add_argument('user-agent=' + user_agent)
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    options.add_experimental_option('prefs', {
-        # "download.default_directory": "다운로드 받고자 하는 폴더 경로",
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True})
+#파일 리스트 인자로 받아서 URL 연결
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(options=options)
+    #명시적 대기
+    driver.implicitly_wait(3)
+    return driver
 
-    #URL 연결 
-    url_link = 'https://www.virustotal.com/gui/home/upload'
-    driver.get(url_link)
-    time.sleep(3)
+def main(file, option):
+    driver=get_driver()
+    if option == 'default':#URL 연결 
+        for  i in range(len(file)):
+            url_link = 'VT 링크'.format(file[i][:-4])
+            driver.get(url_link)
+            shadow = Shadow(driver)
+            # 명시적 대기
+            shadow.set_explicit_wait(5)
+            First_submission_Time = shadow.find_element("div > div > div:nth-child(1) > div > a:nth-child(2)")
+            print("First Submission Time : " + str(First_submission_Time))
+    else:
+        for  i in range(len(file)):
+            #옵션에 따른 창 접근 
+            link_info = 'VT 링크'.format(file[i][:-4])
+            url_link = link_info + str(option)
+            driver.get(url_link)
+            print("============================================")
+            print("###" + str(url_link) + "접속...###")
+            shadow = Shadow(driver)
+            # 명시적 대기
+            shadow.set_explicit_wait(20, 5)
+            shadow.find_element_by_xpath('//*[@id="report"]//div/div[2]/div/ul/li[3]/a').click()
+            First_submission_Time = shadow.find_element("vt-ui-key-val-table")
+            print("First Submission Time : " + str(First_submission_Time.text))
 
-    #검색 창 열기
-    driver.find_element(By.XPATH, '//*[@id="view-container"]/home-view//div/div/div/div[1]/div[1]/ul/li[3]/a').click()
-
-    #파일 정보 얻길 원하는 해시값 입력 
-    query = '해시값'
-    search_tab = driver.find_element(By.CSS_SELECTOR, '#searchInput')
-    search_tab.send_keys(query)
-    search_tab.send_keys(Keys.ENTER)
-
-
+def file_list(file_path):
+    print("============================================")
+    print("###파일 목록 수집 중...###")
+    fl = os.listdir(file_path)
+    print("###파일 목록 수집 완료!###")
+    return fl
 
 if __name__ == '__main__':
-    main_crawling()
+    apk_file_path = "APK 파일 존재 경로"
+    fl = file_list(apk_file_path)
+    main(fl, 'details')
