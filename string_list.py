3a4e88d884ec19df5339105196b5edb162114a63
byeongyeol <101905747+byeongyeolahn@users.noreply.github.com>
2022-11-06 04:11:13 +0900

    Create string_list.py
    파일 명, 벤더사 등 리스트

---
+++ string_list.py
@@ -0,0 +1,24 @@
+
+
+def vendor_list():
+    company_list = ['AhnLab-V3', 'Avira (no cloud)', 'BitDefenderFalx', 'Comodo', 'Cynet', 'Cyren', 'ESET-NOD32', 'Fortinet', 'Google', 'Ikarus', 
+                    'Jiangmin', 'K7GW', 'Kaspersky', 'Lionic', 'MaxSecure', 'McAfee', 'McAfee-GW-Edition', 'Microsoft', 'NANO-Antivirus',
+                    'QuickHeal', 'Rising', 'Sophos', 'Symantec', 'Symantec Mobile Insight', 'Tencent', 'Trustlook', 'VirIT', 'ZoneAlarm by Check Point',
+                    'Acronis (Static ML)', 'Ad-Aware', 'Alibaba', 'ALYac', 'Antiy-AVL', 'Arcabit', 'Avast', 'Avast-Mobile', 'Baidu', 'BitDefender',
+                    'BitDefenderTheta', 'Bkav Pro', 'ClamAV', 'CMC', 'DrWeb', 'Emsisoft', 'eScan', 'F-Secure', 'GData', 'Gridinsoft (no cloud)',
+                    'K7AntiVirus', 'Kingsoft', 'Malwarebytes', 'MAX', 'Panda', 'Sangfor Engine Zero', 'SUPERAntiSpyware', 'TACHYON', 'Trellix (FireEye)',
+                    'TrendMicro', 'TrendMicro-HouseCall', 'VBA32', 'VIPRE', 'ViRobot', 'Yandex', 'Zillya', 'Zoner', 'CrowdStrike Falcon',
+                    'Cybereason', 'Cylance', 'Elastic', 'Palo Alto Networks', 'SecureAge', 'SentinelOne (Static ML)', 'TEHTRIS', 'Trapmine',
+                    'Webroot']
+    return company_list
+
+def detail_string():
+    detail_title = ['MD5', 'SHA-1', 'SHA-256', 'Vhash', 'SSDEEP', 'TLSH', 'File type', 'Magic', 'File size', 'First Submission',
+                    'Last Submission', 'Last Analysis', 'Earliest Contents Modification', 'Latest Contents Modification', 'Names', 'Package Name',
+                    'Main Activity', 'Internal Version', 'Displayed Version', 'Minimum SDK Version', 'Valid From', 'Valid To' , 'Serial Number', 'Thumbprint', 'Distinguished Name',
+                    'Permissions', 'Activities', 'Services', 'Receivers', 'Providers', 'Intent Filters By Action', 'Interesting Strings' ]
+    return detail_title
+
+def classification_list():
+    mal_tag = ['Adware', 'Ransomware', 'Spyware', 'Dropper', 'Banker', 'Phishing', 'SMS', 'Backdoor']
+    return mal_tag
