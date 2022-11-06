41168f3fe45fa1e227866bca487fcb71131da09b
byeongyeol <101905747+byeongyeolahn@users.noreply.github.com>
2022-11-07 02:40:49 +0900

    Update main_crawling.py
    Json 변환 로직 추가

--- main_crawling.py
+++ main_crawling.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from pyshadow.main import Shadow
 import time
 import os
-import argparse
 import string_list
 import shutil
+import json
 
 # parser = argparse.ArgumentParser(description="사용법 test 입니다")
 # parser.add)argument('-')
@@ -19,8 +16,10 @@ CHROMEDRIVER_PATH = 'C:\\Users\\quddu\\\Desktop\\뺑열\\\Coding\\CHROMEDRIVER\\
 
 def get_driver():
     options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # options.add_argument('window-size = 1920x1080')
+    # options.add_argument('disable-gpu')
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
     driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
     #명시적 대기
     driver.implicitly_wait(3)
@@ -36,13 +35,13 @@ def main_crawling(file, option):
             link_info = 'https://www.virustotal.com/gui/file/{}'.format(file[i][:-4])
             driver.get(link_info)
             print("================================================================================")
-            print("[+]" + str(link_info) + "접속")  
+            print("[+] " + str(link_info) + " 접속")  
         
         else:
             url_link = link_info + str(option)
             driver.get(url_link)
             print("================================================================================")
-            print("[+]" + str(url_link) + "접속")
+            print("[+]" + str(url_link) + " 접속")
         
         #Shadow 객체 생성
         shadow = Shadow(driver)
@@ -95,7 +94,7 @@ def crawling_parse(file_name, data, option):
                 vender_index = data.index(vendor_list[vl])
                 vendor_value.append(data[vender_index+1])
             except:
-                print("[+]" + str(vendor_list[vl] + "이(가) 존재하지 않음"))
+                print("[+] " + str(vendor_list[vl] + "이(가) 존재하지 않음"))
         return Determining_Malware(file_name, vendor_list, vendor_value)
         
         print(vendor_value)
@@ -120,10 +119,10 @@ def Determining_Malware(file_name, company, detection_name):
         print()
     else:
         shutil.move(start_path, target_path)
-    print("[+]" + str(file_name) + "분류(악성) 완료")
+    print("[+] " + str(file_name) + "분류(악성) 완료")
 
-
-
+    # dic로 전환
+    list_to_dictionary(file_name, company, detection_name)
 
 def create_dec(tag_list):
     if os.path.isdir('./mal_apk') and os.path.isdir('./benign_apk'):
@@ -131,6 +130,7 @@ def create_dec(tag_list):
     else:
         os.mkdir('./mal_apk')
         os.mkdir('./benign_apk')
+        os.mkdir('./json_dic')
         for ct in range(len(tag_list)):
             dic_name = './mal_apk/{}'.format(tag_list[ct])
             os.mkdir(dic_name)
@@ -138,7 +138,21 @@ def create_dec(tag_list):
         print("================================================================================")
         print("[+] 디렉토리 생성 완료")
 
+def list_to_dictionary(file_name, company, detection_name):
+    vendor_list = company
+    if vendor_list > detection_name:
+        dictionary = {vendor_list[d] : detection_name[d] for d in range(len(vendor_list))}
+    else:
+        dictionary = {vendor_list[d] : detection_name[d] for d in range(len(detection_name))}
+
+    slice_filename = file_name[:-4]
+    json_name = "./json_dic/" + str(slice_filename) + ".json"
+    with open(json_name, "w") as json_file:
+        json.dump(dictionary, json_file, indent=4, sort_keys=True)
+    print("[+] " + str(json_name) + ".json 파일 변환 완료")
 # def Mal_Classification(file_path, detection_name):
+    # vendor_top_15 = string_list.vendor_top_15()
+
     
 
 if __name__ == '__main__':
@@ -152,4 +166,5 @@ if __name__ == '__main__':
 
     crawling_data = main_crawling(fl, 'detection')
     print("총 걸린 분류 시간 :", time.time() - start)
+    print("[+] 작업 완료")
     #파서 실행
